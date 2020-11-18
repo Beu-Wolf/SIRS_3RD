4d309d380815b2a5af2c5a3eaf4a07d8f5aec68a
Beu-Wolf <daniel.gseara@gmail.com>
2020-11-18 11:09:07 +0000

    TLS between client and Server

--- Server/src/main/java/sirs/server/MainServer.java
+++ Server/src/main/java/sirs/server/MainServer.java
@@ -1,41 +1,102 @@
 package sirs.server;
 
 import javax.crypto.spec.SecretKeySpec;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.security.PublicKey;
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.TreeMap;
 
 public class MainServer {
+
     private TreeMap<String, ClientInfo> _clients = new TreeMap<>();
     private ArrayList<FileInfo> _files = new ArrayList<>();
 
 
+
     private String _host;
     private int _port;
+    private char[] _password;
 
     public MainServer(String host, int port) {
         _host = host;
         _port = port;
+        _password = "changeit".toCharArray();
+
     }
 
-    public void close() {
-        System.out.println("Closing");
-    }
-
-    public void run() {
-        System.out.println("Running at " + _host + ":" + _port);
-        // Initiate Server
-        Scanner scanner = new Scanner(System.in);
-
-        while (scanner.nextLine().length() != 0){
-            // do classic stuff
+    public void handleComms(SSLSocket s) {
+        System.out.println("accepted");
+        try {
+            InputStream is = new BufferedInputStream(s.getInputStream());
+            OutputStream os = new BufferedOutputStream(s.getOutputStream());
+            byte[] data = new byte[2048];
+            int len = is.read(data);
+            if (len <= 0) {
+                throw new IOException("no data received");
+            }
+            System.out.printf("server received %d bytes: %s%n",
+                    len, new String(data, 0, len));
+            os.write(data, 0, len);
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        close();
+
     }
+
+
+    private SSLServerSocketFactory getServerSocketFactory() {
+        SSLServerSocketFactory ssf;
+        try {
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            KeyStore ks = KeyStore.getInstance("PKCS12");
+
+            ks.load(new FileInputStream("keys/server.keystore.pk12"), _password);
+            kmf.init(ks, _password);
+
+            KeyStore ksTrust = KeyStore.getInstance("PKCS12");
+            ksTrust.load(new FileInputStream("keys/server.truststore.pk12"), _password);
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ksTrust);
+
+            ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            ssf = ctx.getServerSocketFactory();
+            return ssf;
+        } catch (NoSuchAlgorithmException | KeyStoreException | CertificateException | IOException | UnrecoverableKeyException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void start() {
+        SSLServerSocketFactory ssl = getServerSocketFactory();
+
+        assert ssl != null;
+        try(SSLServerSocket socket = (SSLServerSocket) ssl.createServerSocket(_port)) {
+            String[] protocols = new String[] {"TLSv1.3"};
+            String[] cipherSuites = new String[] {"TLS_AES_128_GCM_SHA256"};
+            socket.setEnabledProtocols(protocols);
+            socket.setNeedClientAuth(true);
+            socket.setEnabledCipherSuites(cipherSuites);
+
+            System.out.println("Running at " + _host + ":" + _port);
+
+            while (true) {
+                SSLSocket s = (SSLSocket) socket.accept();
+                handleComms(s);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 
     public void receiveUserKey(String url, PublicKey publicKey, String username) { // Can also receive the message here and parse in this function
         _clients.put(username, new ClientInfo(url, publicKey, username));
--- Server/src/main/java/sirs/server/RRRDServer.java
+++ Server/src/main/java/sirs/server/RRRDServer.java
@@ -2,12 +2,12 @@ package sirs.server;
 
 public class RRRDServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         String host = "localhost";
         int port = 10000;
 
         MainServer server = new MainServer(host, port);
-        server.run();
+        server.start();
 
     }
 }
