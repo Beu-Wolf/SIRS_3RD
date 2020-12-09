6f9628cb0eb1884be6e8c17f043a85e14165ed8a
Beu-Wolf <daniel.gseara@gmail.com>
2020-12-09 18:12:59 +0000

    Use console in all commands

--- Client/src/main/java/Client/Client.java
+++ Client/src/main/java/Client/Client.java
@@ -42,7 +42,7 @@ public class Client {
     }
 
     public void interactive() {
-        Scanner scanner = new Scanner(System.in);
+        Console clientConsole = System.console();
         try {
             connectToServer();
             ObjectOutputStream os = new ObjectOutputStream(_currentConnectionSocket.getOutputStream());
@@ -50,7 +50,7 @@ public class Client {
             while(true) {
                 System.out.print("> ");
 
-                    String command = scanner.nextLine().trim();
+                    String command = clientConsole.readLine().trim();
                     if (Pattern.matches("^exit$", command)) {
                         parseExit(os, is);
                         if(_currentConnectionSocket != null) {
@@ -58,19 +58,19 @@ public class Client {
                         }
                         break;
                     } else if (Pattern.matches("^register$", command)) {
-                        parseRegister(scanner, os, is);
+                        parseRegister(clientConsole, os, is);
                     } else if (Pattern.matches("^login$", command)) {
-                        parseLogin(scanner, os, is);
+                        parseLogin(clientConsole, os, is);
                     } else if (Pattern.matches("^create file$", command)) {
-                        parseCreateFile(scanner, os, is);
+                        parseCreateFile(clientConsole, os, is);
                     } else if (Pattern.matches("^get file$", command)) {
-                        parseGetFile(scanner, os, is);
+                        parseGetFile(clientConsole, os, is);
                     } else if (Pattern.matches("^edit file$", command)) {
-                        parseEditFile(scanner, os, is);
+                        parseEditFile(clientConsole, os, is);
                     } else if (Pattern.matches("^share file$", command)) {
-                        parseShareFile(scanner, os, is);
+                        parseShareFile(clientConsole, os, is);
                     } else if (Pattern.matches("^get shared$", command)) {
-                        parseGetShared(scanner, os, is);
+                        parseGetShared(clientConsole, os, is);
                     }
 
 
@@ -78,16 +78,15 @@ public class Client {
         } catch (NoSuchAlgorithmException | KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | KeyManagementException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-        scanner.close();
 
     }
-    public void parseLogin(Scanner scanner, ObjectOutputStream os, ObjectInputStream is) throws IOException, ClassNotFoundException {
+    public void parseLogin(Console clientConsole, ObjectOutputStream os, ObjectInputStream is) throws IOException, ClassNotFoundException {
 
-        System.out.println("Please enter your username");
-        String username = scanner.nextLine().trim();
+        System.out.print("Please enter your username: ");
+        String username = clientConsole.readLine().trim();
         _username = username;
-        System.out.println("Please enter your password");
-        String password = scanner.nextLine().trim();
+        System.out.print("Please enter your password: ");
+        String password = String.valueOf(clientConsole.readPassword());
 
         login(username, password, os, is);
     }
@@ -110,13 +109,13 @@ public class Client {
         System.out.println("Result: " + reply.get("response").getAsString());
     }
 
-    public void parseRegister(Scanner scanner, ObjectOutputStream os, ObjectInputStream is) throws IOException, KeyStoreException, ClassNotFoundException, CertificateException, NoSuchAlgorithmException {
-        System.out.println("Please enter your username");
-        String username = scanner.nextLine().trim();
-        System.out.println("Please enter your password");
-        String pw1 = scanner.nextLine().trim();
-        System.out.println("Please re-enter your password to confirm");
-        String pw2 = scanner.nextLine().trim();
+    public void parseRegister(Console clientConsole, ObjectOutputStream os, ObjectInputStream is) throws IOException, KeyStoreException, ClassNotFoundException, CertificateException, NoSuchAlgorithmException {
+        System.out.print("Please enter your username: ");
+        String username = clientConsole.readLine().trim();
+        System.out.print("Please enter your password: ");
+        String pw1 = String.valueOf(clientConsole.readPassword());
+        System.out.print("Please re-enter your password to confirm: ");
+        String pw2 = String.valueOf(clientConsole.readPassword());
         if (!pw1.equals(pw2)) {
             System.out.println("Passwords don't match");
         }
@@ -162,9 +161,9 @@ public class Client {
     }
 
 
-    public void parseCreateFile(Scanner scanner, ObjectOutputStream os, ObjectInputStream is) throws NoSuchAlgorithmException {
+    public void parseCreateFile(Console clientCosole, ObjectOutputStream os, ObjectInputStream is) throws NoSuchAlgorithmException {
         System.out.print("Please enter file path (from the " + _filesDir + " directory): ");
-        String path = scanner.nextLine().trim();
+        String path = clientCosole.readLine().trim();
         System.out.println("Filename: " + path);
         try {
             createFile(path, os, is);
@@ -223,9 +222,9 @@ public class Client {
         System.out.println("Operation Successful");
     }
 
-    public void parseGetFile(Scanner scanner, ObjectOutputStream os, ObjectInputStream is) {
+    public void parseGetFile(Console clientConsole, ObjectOutputStream os, ObjectInputStream is) {
         System.out.print("Please enter file path to get (from the " + _filesDir + " directory): ");
-        String path = scanner.nextLine().trim();
+        String path = clientConsole.readLine().trim();
         System.out.println("Filename: " + path);
 
         try {
@@ -300,9 +299,9 @@ public class Client {
         return cipher.doFinal(chunk);
     }
 
-    public void parseEditFile(Scanner scanner, ObjectOutputStream os, ObjectInputStream is) {
+    public void parseEditFile(Console clientConsole, ObjectOutputStream os, ObjectInputStream is) {
         System.out.print("Please enter file path to edit (from the " + _filesDir + " directory): ");
-        String path = scanner.nextLine().trim();
+        String path = clientConsole.readLine().trim();
         System.out.println("Filename: " + path);
 
         try {
@@ -358,11 +357,11 @@ public class Client {
         System.out.println("Operation Successful");
     }
 
-    public void parseShareFile(Scanner scanner, ObjectOutputStream os, ObjectInputStream is) {
-        System.out.print("Please enter file path to share (from the files directory): ");
-        String path = scanner.nextLine().trim();
+    public void parseShareFile(Console clientConsole, ObjectOutputStream os, ObjectInputStream is) {
+        System.out.print("Please enter file path to share (from the " + _filesDir + " directory): ");
+        String path = clientConsole.readLine().trim();
         System.out.print("Share with user: ");
-        String username = scanner.nextLine().trim();
+        String username = clientConsole.readLine().trim();
 
         try {
             shareFile(path, username, os, is);
@@ -418,7 +417,7 @@ public class Client {
         System.out.println("Operation Successful!");
     }
 
-    public void parseGetShared(Scanner scanner, ObjectOutputStream os, ObjectInputStream is) {
+    public void parseGetShared(Console clientConsole, ObjectOutputStream os, ObjectInputStream is) {
         System.out.println("Getting shared files...");
 
         try {
